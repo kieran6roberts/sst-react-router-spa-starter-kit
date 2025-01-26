731e44f9a18011cf5095e0134168088d6d288a95
kieran <kieran6roberts@gmail.com>
2025-01-26 14:17:59 +0000

    feat: add web infra logic

---
+++ config/index.ts
@@ -0,0 +1,7 @@
+/*
+
+Export your app config from here, feel free to move it elsewhere.
+
+export const DOMAIN = "example.com";
+
+*/
--- infra/web.ts
+++ infra/web.ts
@@ -1,17 +1,21 @@
-// import { config } from "../config";
+export const frontend = new sst.aws.StaticSite("Frontend", {
+  path: "packages/frontend",
+  build: {
+    output: "build/client",
+    command: "pnpm build",
+  },
+  indexPage: "index.html",
+  
+  /*
+   Once you've set up your domain in Route 53, you can uncomment this and add your domain.
+   You can do this anytime after initial deployment.
+  */
 
-// export const frontend = new sst.aws.StaticSite("Frontend", {
-//   path: "packages/frontend",
-//   build: {
-//     output: "build/client",
-//     command: "pnpm build",
-//   },
-//   indexPage: "index.html",
-//   domain:
-//   $app.stage === 'production'
-//     ? {
-//         name: config.domain,
-//         redirects: [`www.${config.domain}`],
-//       }
-//     : undefined,
-// });
+  // domain:
+  // $app.stage === 'production'
+  //   ? {
+  //       name: config.domain,
+  //       redirects: [`www.${config.domain}`],
+  //     }
+  //   : undefined,
+});
